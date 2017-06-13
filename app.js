e31aca843a4c05bb3860cfad1419500bde4fe9bf
Hano <hinsul3113@gmail.com>
2017-06-13 13:45:17 -0700

    used scss instead of sass and ignored compiled css files

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// This is where we are setting our routers
 app.use('/', index);
 app.use('/users', users);
 
