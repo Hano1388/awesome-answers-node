7916a47a6f6620ce0bb26b10074dca226751d7ae
Hano <hinsul3113@gmail.com>
2017-06-14 13:48:23 -0700

    Add questions#new, questions#create and navbar

--- routes/questions.js
+++ routes/questions.js
@@ -1,50 +1,102 @@
 const express = require('express');
 const router = express.Router();
+
 const { Question } = require('../models');
 
-// Question#index PATH: /question/ METHOD: GET
+// questions#index PATH: /questions/ METHOD: get
 router.get('/', (req, res) => {
   Question
     .findAll()
     .then(questions => {
       // passing a second argument to the render of the response
-      // object will make available all its properties as variables
-      // inside the template
-      res.render('questions/index', { questions: questions });
+      // object will make available all its properties
+      // as variables inside the template
+      res.render('questions/index', {questions: questions});
     });
 });
 
-// questions#show PATH: /questions/:id METHOD: GET
-// NOTE: when defined a function, prefix with the 'async'
+// questions#new PATH: /questions/new METHOD: get
+router.get('/new', (req, res) => {
+  const question = Question.build();
+  res.render('questions/new', {question: question});
+});
+
+// questions#create PATH: /questions METHOD: post
+router.post('/', (req, res) => {
+  // We destructure the values of the form inputs
+  // from req.body to make sure that we only get attributes
+  // that we want for creating a Question
+  const { title, description } = req.body
+  Question
+    .create({ title, description })
+  // ð is syntax sugar for ð
+  // { title: title, description: description }
+    .then(question => {
+      res.redirect(`/questions/${question.id}`);
+    })
+    .catch(error => {
+      next(error);
+    })
+});
+
+// questions#show PATH: /questions/:id METHOD: get
+// when declaring a function, prefix with the `async`
 // keyword to make it an async/await function.
 // async/await functions can treat Promises as if
 // they are synchronous.
-// They always return their value wrapped in a promise.
-// In other words, the return will be the resolved value
-// of the promise
-router.get('/:id', async (req, res) => {
-  // To get params from your url (i.e /:id),
-  // grab them from the params property on the request
-  // object as shown
+// They always return their value wrapped in a Promise. In
+// other words, the return value will be the resolved value
+// of the promise.
+
+// async function () {}
+router.get('/:id', async (req, res, next) => {
+  // To get params from your url (i.e. /:id),
+  // grab them the params property on the request object
+  // as shown below ð
   const { id } = req.params;
-  const question = await Question.findById(id);
-  res.render('questions/show', {question: question});
-
-})
-
-// 👇 version of the above ☝️ without using async/await
-// router.get('/:id', async (req, res) => {
-//   // To get params from your url (i.e /:id),
-//   // grab them from the params property on the request
-//   // object as shown
-//   const { id } = req.params;
-//   const question = Question
-//   .findById(id)
-//   .then(question => {
-//    res.render('questions/show', {question: question});
-//   })
-// })
+  // try .. catch block is javascript from catching errors.
+  // In other words, we can use this syntax preventing our
+  // application from crashing when error occurs
+  try {
+    // put the code that might crash inside of the try block
+    const question = await Question.findById(id);
+    res.render('questions/show', {question: question});
+  } catch (error) {
+    // the `error` variable will hold the error object
+    // describing what happened
+    // if it craches, do something here instead
+    next(error);
+  }
+});
 
 
+// ð version of the above ð route without an
+// async/await function
+/*
+router.get('/:id', (req, res, next) => {
+  // To get params from your url (i.e. /:id),
+  // grab them the params property on the request object
+  // as shown below ð
+  const { id } = req.params;
+  const question = Question
+    .findById(id)
+    .then(question => {
+      res.send(question);
+    })
+    .catch(error => {
+      next(error);
+    })
+});
+*/
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+//
